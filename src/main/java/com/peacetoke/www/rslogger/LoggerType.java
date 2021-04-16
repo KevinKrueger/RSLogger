eb4d56245108937d684cccf002ee13bacf68fc69
Kevin Krüger <Kevin22042001@gmail.com>
2021-04-16 09:01:28 +0200

    (Prepare Version 1.4.6)
    (New Functionality)
    -> debug
    
    (Changed Functionality in RSLogger)
    -> getDeviceMemory => getCurrentDeviceInfo
    
    (Changed Structure)
    
    (Gradle)
    -> JavaDoc was added

---
+++ src/main/java/com/peacetoke/www/rslogger/LoggerType.java
@@ -0,0 +1,14 @@
+package com.peacetoke.www.rslogger;
+
+/**
+ * Described the Type of the Log.
+ **/
+public enum LoggerType
+{
+    INFO,
+    ERROR,
+    WARN,
+    DEBUG,
+    DONE,
+    ANALYSES
+};
